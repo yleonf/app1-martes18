eb26385ba776778030b2be99a79e5d48ea6ac893
yasna <yleon@gmail.com>
2024-06-18 14:04:21 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import Saludo from './Saludo'
+function App() {
+ 
+
+  return (
+    <Saludo nombre='juan' apellido='León'/>
+  )
+}
+
+export default App
---
+++ src/Saludo.jsx
@@ -0,0 +1,13 @@
+function Saludo(props){
+
+    return(
+    <>
+    <h1>Hola</h1>
+    <h1>Como estas {props.nombre} {props.apellido}?</h1>
+    </>
+         
+      
+       
+    )
+}
+export default Saludo;
